c28bd3f750580ac60c3cf9eadd368bbde21fdf88
Connor Gilhuly, or Cratmang <cratmang@gmail.com>
2021-09-27 22:57:00 -0400

    Initial commit, 8o file contains data for (monocoloured) tiles and part of the world map.

---
+++ OctomaTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="OctomaTileset" tilewidth="8" tileheight="8" tilecount="121" columns="11">
+ <image source="sprites.png" width="88" height="88"/>
+</tileset>
